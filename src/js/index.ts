d9b8cc6d6066abdd67f498c94e89bc6b893c54e4
vladandrei228 <58209761+vladandrei228@users.noreply.github.com>
2020-09-22 15:00:21 +0200

    first 2 tasks

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,30 @@
 //Write your Typescript code here
+console.log("Task 1")
+let firstElem: HTMLLIElement = <HTMLLIElement>document.getElementById("3");
+console.log(firstElem);
+
+let firstList: HTMLCollectionOf = <HTMLCollectionOf>document.getElementsByTagName("li");
+console.log("Li type list");
+console.log(firstList);
+
+let firstClassNAme: HTMLCollectionOf = <HTMLCollectionOf>document.getElementsByClassName("unhealthy");
+console.log("Class type list:");
+console.log(firstClassNAme);
+
+console.log("Task 2")
+for (let index = 0; index < firstList.length; index++) {
+    const element = firstList[index];
+    console.log(element)
+    if(element.hasAttribute("class"))
+    {
+        if(element.getAttribute("class")=="healthy" )
+        {
+            element.setAttribute("class","unhealthy");
+        }
+        else
+        {
+            element.setAttribute("class","healthy")
+        }
+
+    }
+}
