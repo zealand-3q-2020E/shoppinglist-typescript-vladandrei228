d559eb5d3697894ee31b9313c2aea5cc6add442b
vladandrei228 <58209761+vladandrei228@users.noreply.github.com>
2020-09-28 21:18:34 +0200

    tasks until task 8

--- src/js/index.ts
+++ src/js/index.ts
@@ -11,7 +11,7 @@ let firstClassNAme: HTMLCollectionOf = <HTMLCollectionOf>document.getElementsByC
 console.log("Class type list:");
 console.log(firstClassNAme);
 
-console.log("Task 2")
+console.log("Task 2 & 3")
 for (let index = 0; index < firstList.length; index++) {
     const element = firstList[index];
     console.log(element)
@@ -28,3 +28,48 @@ for (let index = 0; index < firstList.length; index++) {
 
     }
 }
+
+
+console.log("Task 4")
+let parent = document.getElementById("list").parentNode.nodeName;
+console.log(parent);
+let fchild = document.getElementById("list").firstChild.TEXT_NODE;
+console.log(fchild);
+let lchild = document.getElementById("list").lastChild.nodeType;
+console.log(lchild);
+
+
+console.log("Task 5")
+let listunhealthy:HTMLUListElement = <HTMLUListElement>document.getElementById("listUnhealthy")
+let newLi:HTMLLIElement = <HTMLLIElement>document.createElement("li")
+newLi.setAttribute("class", "unhealthy")
+let newText = document.createTextNode("chips")
+newLi.appendChild(newText)
+listunhealthy.appendChild(newLi)
+
+
+console.log("Task 6")
+let newItem = document.createElement("li")
+newItem.setAttribute("class", "healthy")
+let newTxt = document.createTextNode("Water")
+newItem.appendChild(newTxt)
+
+let list = document.getElementById("list")
+list.insertBefore(newItem, list.childNodes[3])
+
+console.log("Task 7")
+let mover
+for (let index = 0; index < firstList.length; index++)
+{
+    const element = firstList[index];
+    if (element.hasAttribute("class")){
+        if(element.getAttribute("class")== "unhealthy"){
+            mover = element;
+            listunhealthy.insertBefore(mover, listunhealthy.childNodes[0]);
+            firstList.removeChild(firstList.childNodes[index]);
+        }
+    }
+}
+
+console.log("Task 8")
+document.getElementById("0").textContent = "bananas";
